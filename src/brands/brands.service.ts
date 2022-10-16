663b9048808ef3407624a11f87bdd1826a3af3d3
Alan Rodriguez <alan.rodriguez193@tectijuana.edu.mx>
2022-10-15 17:53:00 -0700

    CRUD Brands agregado

---
+++ src/brands/brands.service.ts
@@ -0,0 +1,55 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateBrandDto } from './dto/create-brand.dto';
+import { UpdateBrandDto } from './dto/update-brand.dto';
+import { Brand } from './entities/brand.entity';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class BrandsService {
+  create(createBrandDto: CreateBrandDto) {
+    const brand: Brand = {
+      id: uuid(),
+      name: createBrandDto.name.toLocaleLowerCase(),
+      createdAt: new Date().getTime(),
+    };
+    this.brands.push(brand);
+
+    return brand;
+  }
+
+  private brands: Brand[] = [
+    {
+      id: uuid(),
+      name: 'Toyota',
+      createdAt: new Date().getTime(),
+    },
+  ];
+
+  findAll() {
+    return this.brands;
+  }
+
+  findOne(id: string) {
+    const brand = this.brands.find((brand) => brand.id === id);
+    if (!brand) throw new NotFoundException(`Brand #${id} not found`);
+    return brand;
+  }
+
+  update(id: string, updateBrandDto: UpdateBrandDto) {
+    let branDB = this.brands.find((brand) => brand.id === id);
+
+    this.brands = this.brands.map((brand) => {
+      if (brand.id === id) {
+        branDB.updatedAt = new Date().getTime();
+        branDB = { ...branDB, ...updateBrandDto };
+        return branDB;
+      }
+      return brand;
+    });
+    return branDB;
+  }
+
+  remove(id: string) {
+    this.brands = this.brands.filter((brand) => brand.id !== id);
+  }
+}
